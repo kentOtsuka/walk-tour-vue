b6de01b82195d6d41d7d73374e99f0d9fdff1f01
大塚健登 <kent0tsuka1118@gmail.com>
2022-07-07 00:00:39 +0900

    Fix：マイページにお気に入り解除機能を実装 #16 #17

--- src/pages/UserProfile.vue
+++ src/pages/UserProfile.vue
@@ -24,36 +24,76 @@
     </v-row>
 
     <h2 class="mb-3 d-flex align-center justify-center">
-      <v-icon left bottom color="pink">mdi-cards-heart</v-icon>
+      <v-icon left bottom color="pink">mdi-heart</v-icon>
       Favorite Spot
     </h2>
+
     <v-divider class="mb-2" style="max-width: 700px; margin: auto;"></v-divider>
-    <div class="d-flex justify-center">
-      <v-row style="max-width: 1000px; margin: auto;">
-        <v-col v-for="spotDetail in spotDetails" :key="spotDetail.id" cols="12" lg="6" md="12" sm="12">
-          <v-hover v-slot="{ hover }">
-            <v-card :elevation="hover ? 12 : 2" max-width="500px" style="margin: auto;">
-              <div class="d-flex justify-space-between">
-                <div class="d-flex flex-column">
-                  <v-list-item style="width: 160px;">
+
+    <!-- お気に入り登録されているスポットがある時に表示 -->
+    <template v-if="spotDetails.length != 0">
+      <div class="d-flex justify-center">
+        <!-- 画面幅がxs,smの時に表示 -->
+        <v-row class="mx-auto hidden-md-and-up">
+          <v-col v-for="spotDetail in spotDetails" :key="spotDetail.id" cols="12" sm="6">
+            <v-hover v-slot="{ hover }">
+              <v-card  :elevation="hover ? 12 : 2" max-width="400px" style="margin: auto;">
+                <v-img :src="spotDetail.video.thumbnail" alt="サムネイル"  @click="openDialog(spotDetail.area, spotDetail.spot, spotDetail.video);" style="cursor: pointer">
+                </v-img>
+                <v-btn class="btn ma-2" fab small color="white" @click="unBookmark(spotDetail.id, spotDetail.spot.id)">
+                  <v-icon color="pink">mdi-heart</v-icon>
+                </v-btn>
+                <div class="d-flex justify-space-between">
+                  <v-list-item>
                     <v-list-item-content>
-                      <v-list-item-title>{{ spotDetail.spot.name }}</v-list-item-title>
+                      <v-list-item-title class="my-1">{{ spotDetail.spot.name }}</v-list-item-title>
                       <v-list-item-subtitle>{{ spotDetail.area.name }}</v-list-item-subtitle>
                     </v-list-item-content>
                   </v-list-item>
-                  <v-divider></v-divider>
-                  <v-btn color="blue darken-1 align-center" class="hidden-sm-and-down" text @click="setSpot(spotDetail.area, spotDetail.spot)">行ってみる！</v-btn>
+                  <v-card-actions>
+                    <v-btn color="blue darken-1 align-center" text  @click="setSpot(spotDetail.area, spotDetail.spot)">行ってみる！</v-btn>
+                  </v-card-actions>
                 </div>
-                <v-img :src="spotDetail.video.thumbnail" alt="サムネイル"  @click="openDialog(spotDetail.area, spotDetail.spot, spotDetail.video);" class="hidden-sm-and-down" style="cursor: pointer"></v-img>
-                <v-card-actions class="hidden-md-and-up">
-                  <v-btn color="blue darken-1 align-center" text @click="setSpot(spotDetail.area, spotDetail.spot)">行ってみる</v-btn>
-                </v-card-actions>
-              </div>
-            </v-card>
-          </v-hover>
-        </v-col>
-      </v-row>
-    </div>
+              </v-card>
+            </v-hover>
+          </v-col>
+        </v-row>
+
+        <!-- 画面幅がmd, lg, xlで表示 -->
+        <v-row class="hidden-sm-and-down" style="max-width: 1000px; margin: auto;">
+          <v-col v-for="spotDetail in spotDetails" :key="spotDetail.id" cols="12" lg="6" md="12" sm="12">
+            <v-hover v-slot="{ hover }">
+              <v-card :elevation="hover ? 12 : 2" max-width="500px" style="margin: auto;">
+                <div class="d-flex justify-space-between">
+                  <div class="d-flex flex-column">
+                    <v-btn class="ma-2" fab small color="white" @click="unBookmark(spotDetail.id, spotDetail.spot.id)">
+                      <v-icon color="pink">mdi-heart</v-icon>
+                    </v-btn>
+                    <v-list-item style="width: 160px;">
+                      <v-list-item-content>
+                        <v-list-item-title>{{ spotDetail.spot.name }}</v-list-item-title>
+                        <v-list-item-subtitle>{{ spotDetail.area.name }}</v-list-item-subtitle>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-divider></v-divider>
+                    <v-btn color="blue darken-1 align-center" text @click="setSpot(spotDetail.area, spotDetail.spot)">行ってみる！</v-btn>
+                  </div>
+                  <v-img :src="spotDetail.video.thumbnail" alt="サムネイル"  @click="openDialog(spotDetail.area, spotDetail.spot, spotDetail.video);" style="cursor: pointer"></v-img>
+                </div>
+              </v-card>
+            </v-hover>
+          </v-col>
+        </v-row>
+      </div>
+    </template>
+
+    <!-- お気に入り登録されているスポットがない時に表示 -->
+    <template v-else>
+      <v-chip label large class="mt-10 d-flex justify-center" color="pink" text-color="white" style="max-width: 700px; margin: auto;">
+        <v-icon left>mdi-heart-plus-outline</v-icon>
+        スポットをお気に入り登録してみよう！
+      </v-chip>
+    </template>
 
     <!-- ダイアログボックス -->
     <v-dialog v-model="dialog" max-width="1200px">
@@ -112,6 +152,7 @@ export default {
     return {
       // すべての地点その国、動画オブジェクトを格納する配列
       spotDetails: [],
+      heart: [],
       // ダイアログに渡すdata
       dialog: false,
       title: '',
@@ -139,18 +180,6 @@ export default {
     ...mapGetters("users", ["authUser"]),
   },
   methods: {
-    // ブックマークに登録されている地点を取得
-    getBookmarks() {
-      axios.get('/bookmarks')
-      .then(res => {
-        for(let i = 0; i < res.data.spots.length; i++) {
-          this.spotDetails.push( {id: i, spot: res.data.spots[i], area: res.data.areas[i], video: res.data.videos[i]} )
-        };
-      })
-      .catch(error => {
-        console.log(error);
-      });
-    },
     // クリックしたカードの地点の国の詳細ページに遷移させる処理
     setSpot(area, spot) {
       // 地点のカウント数を+1する
@@ -169,6 +198,30 @@ export default {
       .catch(error => {
         console.log(error);
       })
+    },
+    // ブックマークに登録されている地点を取得
+    getBookmarks() {
+      axios.get('/bookmarks')
+      .then(res => {
+        for(let i = 0; i < res.data.spots.length; i++) {
+          this.spotDetails.push( {id: i, spot: res.data.spots[i], area: res.data.areas[i], video: res.data.videos[i]} )
+        };
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    },
+    // ブックマーク登録を解除する
+    unBookmark(id, spot) {
+      // お気に入りスポットの配列から削除（非同期処理）
+      this.spotDetails = this.spotDetails.filter((item) => item.id !== id);
+      axios.delete(`/bookmarks/${spot}`)
+      .then(res => {
+        console.log(res.data.status);
+      })
+      .catch(error => {
+        console.log(error);
+      });
     },
     // ダイアログの表示
     openDialog(area, spot, video) {
@@ -195,3 +248,13 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.v-responsive__content{
+  position: relative;
+}
+.btn{
+  position: absolute;
+}
+
+</style>
