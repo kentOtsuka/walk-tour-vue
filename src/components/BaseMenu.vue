319cf849774d4bcde7dc9792976ed7c8a81bcf5e
大塚健登 <kent0tsuka1118@gmail.com>
2022-08-02 22:55:29 +0900

    Add：メニューバーにに多言語化を適用 #40

--- src/components/BaseMenu.vue
+++ src/components/BaseMenu.vue
@@ -8,7 +8,7 @@
             <span class="white--text text-h5">{{ authUser.name.charAt(0) }}</span>
           </v-list-item-avatar>
           <v-list-item-content style="color: #455a64">
-            <v-list-item-title>マイページ</v-list-item-title>
+            <v-list-item-title>{{ $t('menu.my_page') }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -18,19 +18,31 @@
 
       <!-- メニュー表のリスト -->
       <v-list>
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          :to="item.url"
-          @click.native="triggerClick(item.action)"
-          link
-        >
+        <v-list-item to="/" link>
           <v-list-item-icon style="color: #455a64">
-            <v-icon>{{ item.icon }}</v-icon>
+            <v-icon>mdi-home-outline</v-icon>
           </v-list-item-icon>
 
           <v-list-item-content style="color: #455a64">
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item-title>{{ $t('menu.home') }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item to="/spotRanking" link>
+          <v-list-item-icon style="color: #455a64">
+            <v-icon>mdi-fire</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content style="color: #455a64">
+            <v-list-item-title>{{ $t('menu.hot_spot') }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item to="/newsList" link>
+          <v-list-item-icon style="color: #455a64">
+            <v-icon>mdi-information-outline</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content style="color: #455a64">
+            <v-list-item-title>{{ $t('menu.news') }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -40,40 +52,39 @@
 
       <v-list dense>
         <template v-if="!authUser">
-          <v-list-item
-            v-for="item in smallItems"
-            :key="item.title"
-            :to="item.url"
-            @click.native="triggerClick(item.action)"
-            link
-          >
+          <v-list-item to="/login" link>
             <v-list-item-icon style="color: #455a64">
-              <v-icon>{{ item.icon }}</v-icon>
+              <v-icon>mdi-login</v-icon>
             </v-list-item-icon>
 
             <v-list-item-content style="color: #455a64">
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
+              <v-list-item-title>{{ $t('menu.login') }}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </template>
 
         <template v-else>
           <!-- リストを繰り返し処理で全て表示 -->
-          <v-list-item
-            v-for="item in smallItemsLogin"
-            :key="item.title"
-            :to="item.url"
-            @click.native="triggerClick(item.action)"
-            link
-          >
+          <v-list-item to="/spotRequest" link>
             <!-- リストのアイコン -->
             <v-list-item-icon style="color: #455a64">
-              <v-icon>{{ item.icon }}</v-icon>
+              <v-icon>mdi-send-circle</v-icon>
             </v-list-item-icon>
 
             <!-- リストのタイトル -->
             <v-list-item-content style="color: #455a64">
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
+              <v-list-item-title>{{ $t('menu.request_spot') }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item @click.native="triggerClick('logout')" link>
+            <!-- リストのアイコン -->
+            <v-list-item-icon style="color: #455a64">
+              <v-icon>mdi-logout</v-icon>
+            </v-list-item-icon>
+
+            <!-- リストのタイトル -->
+            <v-list-item-content style="color: #455a64">
+              <v-list-item-title>{{ $t('menu.logout') }}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </template>
@@ -92,22 +103,6 @@ export default {
       type: Boolean,
       default: false,
     },
-  },
-  data() {
-    return {
-      // 未ログインのメニューリスト
-      items: [
-        { title: 'トップページ', icon: 'mdi-home-outline', url: '/' },
-        { title: 'ホットスポット', icon: 'mdi-fire', url: '/spotRanking' },
-        { title: 'お知らせ', icon: 'mdi-information-outline', url: '/newsList' },
-      ],
-      smallItems: [{ title: 'ログイン', icon: 'mdi-login', url: '/login' }],
-      // ログイン中のメニューリスト
-      smallItemsLogin: [
-        { title: 'スポットをリクエスト', icon: 'mdi-send-circle', url: '/spotRequest' },
-        { title: 'ログアウト', icon: 'mdi-logout', action: 'logout' },
-      ],
-    };
   },
   computed: {
     // mapGettersでログイン中のユーザを取得
