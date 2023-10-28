3921542e21d0b5b82653015581da268a2d9cc1b2
大塚健登 <kent0tsuka1118@gmail.com>
2023-10-29 00:32:26 +0900

    Fix: お知らせ欄の修正

--- src/pages/NewsList.vue
+++ src/pages/NewsList.vue
@@ -6,27 +6,34 @@
     </h2>
     <v-divider class="mb-2" style="max-width: 700px; margin: auto" />
 
-    <v-simple-table style="max-width: 1000px; margin: auto">
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th class="text-left">{{ $t('news.updated_at') }}</th>
-            <th class="text-left">{{ $t('news.content') }}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="item in newsLists" :key="item.id">
-            <td>{{ item.created_at }}</td>
-            <template v-if="$i18n.locale === 'ja'">
-              <td>{{ item.content }}</td>
-            </template>
-            <template v-if="$i18n.locale === 'en'">
-              <td>{{ item.content_ens }}</td>
-            </template>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
+    <template v-if="newsLists.length == 0">
+      <div class="d-flex justify-center mt-5">
+        <p>{{ $t('news.nothings') }}</p>
+      </div>
+    </template>
+    <template v-else>
+      <v-simple-table style="max-width: 1000px; margin: auto">
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">{{ $t('news.updated_at') }}</th>
+              <th class="text-left">{{ $t('news.content') }}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in newsLists" :key="item.id">
+              <td>{{ item.created_at }}</td>
+              <template v-if="$i18n.locale === 'ja'">
+                <td>{{ item.content }}</td>
+              </template>
+              <template v-if="$i18n.locale === 'en'">
+                <td>{{ item.content_ens }}</td>
+              </template>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </template>
   </v-container>
 </template>
 
@@ -52,7 +59,7 @@ export default {
   methods: {
     getNews() {
       axios.get('/news_lists').then((res) => {
-        this.newsLists = res.data;
+        this.newsLists = res.data.news_lists;
       });
     },
   },
